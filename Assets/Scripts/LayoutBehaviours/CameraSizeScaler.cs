d4646836bd451a9c6a0490e0a770ce58abc035c3
Nicola Racco <nicola@mikamai.com>
2019-09-30 20:57:46 +0200

    handling both portrait and landscape

---
+++ Assets/Scripts/LayoutBehaviours/CameraSizeScaler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pong.LayoutBehaviours
+{
+    public class CameraSizeScaler : MonoBehaviour
+    {
+        private float referenceOrthographicRatio = 19.98674f;
+
+        void Start()
+        {
+            StartCoroutine(UpdateOrthographicSize());
+        }
+
+        IEnumerator UpdateOrthographicSize()
+        {
+            while (true) {
+                float aspectRatio = (float)Screen.height / (float)Screen.width;
+                float orthographicSize = referenceOrthographicRatio * aspectRatio;
+                if (Screen.height > Screen.width) {
+                    orthographicSize /= 2;
+                    if (Camera.main.transform.localRotation.z == 0) {
+                        Camera.main.transform.Rotate(0, 0, 90);
+                    }
+                } else if (Camera.main.transform.localRotation.z != 0) {
+                    Camera.main.transform.Rotate(0, 0, -90);
+                }
+                Camera.main.orthographicSize = orthographicSize;
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+    }
+}
