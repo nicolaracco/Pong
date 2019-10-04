e2ae7bef3c8d3c595ae32d9c29d5f6f0b422a085
Nicola Racco <nicola@mikamai.com>
2019-10-04 23:58:57 +0200

    reduce game area to 4:3 and improve game area scaling

--- Assets/Scripts/LayoutBehaviours/CameraSizeScaler.cs
+++ Assets/Scripts/LayoutBehaviours/CameraSizeScaler.cs
@@ -6,7 +6,7 @@ namespace Pong.LayoutBehaviours
 {
     public class CameraSizeScaler : MonoBehaviour
     {
-        float referenceOrthographicRatio = 19.98674f;
+        float targetAspectRatio = 4f / 3f;
         float portraitRotationAngle;
 
         void Start()
@@ -17,24 +17,49 @@ namespace Pong.LayoutBehaviours
             } else {
                 portraitRotationAngle = 90f;
             }
-            StartCoroutine(UpdateOrthographicSize());
+            StartCoroutine(UpdateViewArea());
         }
 
-        IEnumerator UpdateOrthographicSize()
+        IEnumerator UpdateViewArea()
         {
             while (true) {
-                float aspectRatio = (float)Screen.height / (float)Screen.width;
-                float orthographicSize = referenceOrthographicRatio * aspectRatio;
                 if (Screen.height > Screen.width) {
-                    orthographicSize /= 2;
-                    if (Camera.main.transform.localRotation.z == 0) {
-                        Camera.main.transform.Rotate(0, 0, portraitRotationAngle);
-                    }
-                } else if (Camera.main.transform.localRotation.z != 0) {
-                    Camera.main.transform.Rotate(0, 0, -portraitRotationAngle);
+                    SetPortraitViewArea();
+                } else {
+                    SetLandscapeViewArea();
                 }
-                Camera.main.orthographicSize = orthographicSize;
-                yield return new WaitForSeconds(.1f);
+                yield return new WaitForSeconds(10.25f);
+            }
+        }
+
+        void SetPortraitViewArea()
+        {
+            float windowAspectRatio = (float)Screen.width / (float)Screen.height;
+            // rotate camera by 90 degrees
+            Camera.main.transform.rotation = Quaternion.Euler(0, 0, portraitRotationAngle);
+            // viewport height
+            float scaledHeight = (1f / windowAspectRatio) / targetAspectRatio;
+            if (scaledHeight < 1f) {
+                Camera.main.orthographicSize = 10f * (scaledHeight / windowAspectRatio);
+                Camera.main.rect = new Rect(0, (1f - scaledHeight) / 2f, 1f, scaledHeight);
+            } else {
+                Camera.main.orthographicSize = 10f / windowAspectRatio;
+                Camera.main.rect = new Rect(0, 0, 1, 1);
+            }
+        }
+
+        void SetLandscapeViewArea()
+        {
+            float windowAspectRatio = (float)Screen.width / (float)Screen.height;
+            Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0);
+            Camera.main.orthographicSize = 10f;
+            float scaledHeight = windowAspectRatio / targetAspectRatio;
+
+            if (scaledHeight < 1f) {
+                Camera.main.rect = new Rect(0, (1f - scaledHeight) / 2f, 1f, scaledHeight);
+            } else {
+                float scaledWidth = 1f / scaledHeight;
+                Camera.main.rect = new Rect((1f - scaledWidth) / 2f, 0, scaledWidth, 1f);
             }
         }
     }
